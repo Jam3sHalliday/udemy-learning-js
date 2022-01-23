475ef7e18940e35732341c51b1a3644ccc6131fd
quanggtangg <quanggtangg@gmail.com>
2022-01-23 21:18:48 +0700

    add gitignore, practice DOM

--- guess-my-number/script.js
+++ guess-my-number/script.js
@@ -1,1 +1,9 @@
 'use strict';
+
+//  DOM (document object model): structured representation of html documents allows js to access html elements and styles to manipulate them
+
+document.querySelector(".message").textContent = "Correct Number!";
+document.querySelector(".number").textContent = 13;
+document.querySelector(".score").textContent = 20;
+
+document.querySelector(".guess").value = 33
